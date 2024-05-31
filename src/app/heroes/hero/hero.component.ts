a2d98d6f023c83a106df4ac56904dc83a1bd450a
Marcela Trujillo Toro <mt@fullstacksolvers.com>
2024-05-31 15:51:30 -0500

    first commit

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizedName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age = 110;
+  }
+
+  resetForm(): void {
+    this.age = 45;
+    this.name = 'ironman';
+    // document
+    //   .querySelectorAll('h1')!
+    //   .forEach((element) => (element.innerHTML = '<h1>Desde angular</h1>'));
+  }
+}
